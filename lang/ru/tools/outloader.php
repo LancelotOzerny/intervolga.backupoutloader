998b3660a5d4208d7274c30055bd0f5c12c50d22
Максим Беляков <belyakov@intervolga.ru>
2023-09-11 01:06:13 +0300

    Реализовал логирование этапов выгрузки

---
+++ lang/ru/tools/outloader.php
@@ -0,0 +1,28 @@
+<?php
+// ###############################################################
+// #    MESSAGES
+// ###############################################################
+$MESS['BACKUP_OUTLOAD.MODULE_INCLUDE'] = 'Удалось подключить модуль lancy.backupoutloader';
+$MESS['BACKUP_OUTLOAD.GET_OPTION_DATA'] = 'Получение настроек модуля';
+$MESS['BACKUP_OUTLOAD.FTP_AUTHORISE_SUCCESSFUL'] = 'Удалось пройти FTP вторизацию';
+$MESS['BACKUP_OUTLOAD.PREPARE_TO_OUTLOAD'] = 'Подготовка к выгрузке';
+$MESS['BACKUP_OUTLOAD.PREPARE_CONTAINER_FOLDER'] = 'Подготовка директории-контейнера для выгрузки';
+$MESS['BACKUP_OUTLOAD.DELETE_LIMIT'] = 'Удаление старых резервных копий (вхождение в лимит максимального колическтва резервных копий)';
+$MESS['BACKUP_OUTLOAD.PREPARE_TO_DIR_OUTLOAD'] = 'Подготовка директории выгрузки';
+$MESS['BACKUP_OUTLOAD.START_BACKUP_CREATE'] = 'Создание резервной копии';
+$MESS['BACKUP_OUTLOAD.SUCCESS_BECKUP_CREATED'] = 'Резервная копия создана';
+$MESS['BACKUP_OUTLOAD.START_OUTLOAD'] = 'Выгрузка резервной копии';
+$MESS['BACKUP_OUTLOAD.START_CLEAN'] = 'Начало очистки';
+$MESS['BACKUP_OUTLOAD.DELETE_ALL_BACKUP_BEFORE'] = 'Удаление всех резервных копий до выгрузки';
+$MESS['BACKUP_OUTLOAD.DELETE_ALL_BACKUP_AFTER'] = 'Удаление всех резервных копий после выгрузки';
+$MESS['BACKUP_OUTLOAD.DELETE_NEW_BACKUP'] = 'Удаление новой резервной копии';
+$MESS['BACKUP_OUTLOAD.SUCCESS_OUTLOAD'] = 'Выгрузка успешно завершена';
+
+// ###############################################################
+// #    ERRORS
+// ###############################################################
+$MESS['BACKUP_OUTLOAD.ERROR.TRANSFER_ERROR'] = 'Ошибка передачи части резервной копии';
+$MESS['BACKUP_OUTLOAD.ERROR.BACKUP_CREATE'] = 'Ошибка создания резервной копии: ';
+$MESS['BACKUP_OUTLOAD.ERROR.FTP_AUTHORISE'] = 'Не удалось пройти FTP вторизацию';
+$MESS['BACKUP_OUTLOAD.ERROR.CHECK_SETTING'] = 'Ошибка в настройке данных. Измените настройки модуля';
+$MESS['BACKUP_OUTLOAD.ERROR.MODULE_INCLUDE'] = 'Не удалось подключить модуль';
--- tools/outloader.php
+++ tools/outloader.php
@@ -1,20 +1,23 @@
 <?php
 use \Lancy\BackupOutloader\Backup\BackupController;
 use \Lancy\BackupOutloader\Connection\FtpConnection;
+use \Lancy\BackupOutloader\Log\Logger;
+use Bitrix\Main\Localization\Loc;
 
 set_time_limit(0);
-
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
 if (\Bitrix\Main\Loader::includeModule('lancy.backupoutloader') === false)
 {
-    echo 'Module not included';
-    die();
+    die(Loc::getMessage('BACKUP_OUTLOAD.ERROR.MODULE_INCLUDE'));
 }
+$logger = new Logger();
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.MODULE_INCLUDE'));
 
 // ###############################################################
 // #    GET DATA
 // ###############################################################
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.GET_OPTION_DATA'));
 $options = [
     'connection_host' => COption::GetOptionString('lancy.backupoutloader', 'connection_host'),
     'connection_port' => COption::GetOptionString('lancy.backupoutloader', 'connection_port'),
@@ -31,34 +34,51 @@ $options = [
 // ###############################################################
 // #    CHECK START DATA
 // ###############################################################
-$error = 'Проверьте настройку данных!';
+$error = Loc::getMessage('BACKUP_OUTLOAD.ERROR.CHECK_SETTING');
 
 foreach (['connection_host', 'connection_port', 'connection_login', 'connection_password'] as $option)
 {
-    if (isset($options[$option]) === false || $options[$option] === '') die($error);
+    if (isset($options[$option]) === false || $options[$option] === '')
+    {
+        $logger->Log($error);
+        die($error);
+    }
 }
 
 // ###############################################################
 // #    CONNECTIONS
 // ###############################################################
 $ftp = new FtpConnection($options['connection_host'], $options['connection_port']);
-$ftp->login(
+$login = $ftp->login(
     $options['connection_login'],
     $options['connection_password'],
     isset($options['connection_passive_mode']) && $options['connection_passive_mode'] === 'Y' ? true : false,
 );
 
+if ($login)
+{
+    $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.FTP_AUTHORISE_SUCCESSFUL'));
+}
+else
+{
+    $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.ERROR.FTP_AUTHORISE'));
+    die(Loc::getMessage('BACKUP_OUTLOAD.ERROR.FTP_AUTHORISE'));
+}
+
 // ###############################################################
 // #    PREPARE
 // ###############################################################
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.PREPARE_TO_OUTLOAD'));
 if ($options['outload_remove_all'] === 'before')
 {
+    $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.DELETE_ALL_BACKUP_BEFORE'));
     BackupController::Instance()->deleteAll();
 }
 $backupsBefore = BackupController::Instance()->getList();
 
 if ($options['outload_path'] !== '')
 {
+    $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.PREPARE_CONTAINER_FOLDER'));
     $folder = $options['outload_path'];
     $ftp->createDir($folder);
     $ftp->go($folder);
@@ -93,12 +113,17 @@ if (isset($options['outload_count']) && intval($options['outload_count']) > 0)
     }
 }
 
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.DELETE_LIMIT'));
 foreach ($delete as $delFile)
 {
     $ftp->delete($delFile);
 }
 $folder = 'backup from ' . date('Y-m-d');
 
+// ###############################################################
+// #    WARNING: ДАЛЕЕ ДОБАВИТЬ ОПЦИЮ ЗАМЕНЫ ИЛИ ОТМЕНЫ ВЫГРУЗКИ
+// ###############################################################
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.PREPARE_TO_DIR_OUTLOAD'));
 if($ftp->exist($folder))
 {
     $ftp->delete($folder);
@@ -110,7 +135,18 @@ $ftp->go($folder);
 // ###############################################################
 // #    CREATE
 // ###############################################################
-BackupController::Instance()->create();
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.START_BACKUP_CREATE'));
+$backupCreateResult = BackupController::Instance()->create();
+
+if ($backupCreateResult !== true)
+{
+    $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.ERROR.BACKUP_CREATE') . $backupCreateResult);
+    die(Loc::getMessage('BACKUP_OUTLOAD.ERROR.BACKUP_CREATE') . $backupCreateResult);
+}
+else
+{
+    $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.SUCCESS_BECKUP_CREATED'));
+}
 
 $backupsAfter = BackupController::Instance()->getList();
 $currentBackup = '';
@@ -127,17 +163,26 @@ foreach ($backupsAfter as $backup)
 // ###############################################################
 // #    OUTLOAD
 // ###############################################################
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.START_OUTLOAD'));
 $backupParts = BackupController::Instance()->getAllParts($currentBackup);
 foreach ($backupParts as $part)
 {
-    $ftp->send(BackupController::Instance()->path . '/' . $part, $part);
+    $send = $ftp->send(BackupController::Instance()->path . '/' . $part, $part);
+
+    if ($send === false)
+    {
+        $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.ERROR.TRANSFER_ERROR'));
+        die(Loc::getMessage('BACKUP_OUTLOAD.ERROR.TRANSFER_ERROR'));
+    }
 }
 
 // ###############################################################
 // #    CLEAN
 // ###############################################################
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.START_CLEAN'));
 if ($options['outload_remove_all'] === 'after')
 {
+    $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.DELETE_ALL_BACKUP_AFTER'));
     $backupsAfter = BackupController::Instance()->getList();
     foreach ($backupsAfter as $backup)
     {
@@ -150,8 +195,9 @@ if ($options['outload_remove_all'] === 'after')
 }
 else if ($options['outload_remove_current'] === 'yes')
 {
+    $logger->Log(Loc::getMessage('BACKUP_OUTLOAD.DELETE_NEW_BACKUP'));
     BackupController::Instance()->delete($currentBackup);
 }
 
-echo 'Выгрузка завершена!';
+$logger->Log(Loc::getMessage('BACKUP_OUTLOAD.SUCCESS_OUTLOAD'));
 ?>
